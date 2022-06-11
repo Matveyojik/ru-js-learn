57c534f7bad7a35b5a7d8dfd8a46782a0791f941
argon31 <argon31@gmail.com>
2022-06-11 14:28:50 +0500

    базовый проект

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Изучаем JavaScript на русском
+      </header>
+      <canvas id="доска" className="Draw-desk"></canvas>
+    </div>
+  );
+}
+
+export default App;
